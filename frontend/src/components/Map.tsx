fa7107b7f5e37e24c01960baacef6576acf6e8b7
vandpaolo <110381065+vandpaolo@users.noreply.github.com>
2024-12-30 22:39:10 +0100

    template docs

---
+++ frontend/src/components/Map.tsx
@@ -0,0 +1,37 @@
+import 'leaflet/dist/leaflet.css';
+import { LatLngExpression } from 'leaflet';
+import { MapContainer, Circle, TileLayer } from 'react-leaflet';
+import { useTheme } from '@mui/material/styles';
+
+interface Props {
+  coordinates: [number, number];
+  zoom: number;
+}
+
+const Map = ({ coordinates, zoom }: Props): JSX.Element => {
+  const theme = useTheme();
+  const position: LatLngExpression = coordinates;
+  const fillBlueOptions = {
+    fillColor:
+      theme.palette.mode === 'dark'
+        ? theme.palette.primary.main
+        : theme.palette.success.dark,
+  };
+
+  return (
+    <MapContainer
+      center={position}
+      zoom={zoom}
+      scrollWheelZoom={false}
+      style={{ height: '400px', width: '100%' }}
+    >
+      <TileLayer
+        attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+      />
+      <Circle center={position} pathOptions={fillBlueOptions} radius={50} />
+    </MapContainer>
+  );
+};
+
+export default Map;
